2b892019b7dae2974298790fb94be68825deff29
arijitthehacker <senarijit1618@gmail.com>
2024-05-13 14:11:57 +0530

    feat: initialise repo

---
+++ app/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import Image from "next/image";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import { Button } from "@/components/ui/button"
+import { MantineProvider } from "@mantine/core";
+// import  {header}  from "@/components/ui/header";
+
+import navBar  from "@/components/ui/navBar";
+
+
+export default function Home() {
+  return (
+    <>
+    
+    {navBar()}
+    <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold text-slate-300">Hello there</h1>
+      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+      </>
+
+    
+  );
+
+
+}
+
+
